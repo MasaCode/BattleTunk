1403e9c554c0e66aa77f90792e42c93d5d70fa60
DESKTOP-N0P1S40\masas <masashi.morita@itdcanada.ca>
2016-09-30 11:26:06 -0700

    Initial Commit.

---
+++ BattleTunk/Source/BattleTunk/BattleTunkGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "BattleTunkGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLETUNK_API ABattleTunkGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
