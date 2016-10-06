cc014ecd5e7d0734ef5e170077b76f0c50ea4af8
DESKTOP-N0P1S40\masas <masashi.morita@itdcanada.ca>
2016-10-05 22:03:52 -0700

    I think I am  having a trouble.

--- BattleTunk/Source/BattleTunk/Private/TankBarrel.cpp
+++ BattleTunk/Source/BattleTunk/Private/TankBarrel.cpp
@@ -9,4 +9,11 @@
 void UTankBarrel::Elevate(float RelativeSpeed)
 {
 
+	RelativeSpeed = FMath::Clamp<float>(RelativeSpeed, -1.0f, 1.0f);
+
+	auto ElevationChange = RelativeSpeed * MaxDegreesPerSecond * this->GetWorld()->DeltaTimeSeconds;
+	auto Elevation = FMath::Clamp<float>(RelativeRotation.Pitch + ElevationChange, MinElevation, MaxElevation);
+
+	this->SetRelativeRotation(FRotator(Elevation, 0, 0));
+
 }
